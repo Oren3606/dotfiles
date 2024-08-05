f702bcc98ba0a70d810f83da7d00a4c87d9cb542
Oren3606 <bohako111@gmail.com>
2024-08-05 22:52:40 +0300

    set up lazy.nvim, update fonts in use

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,1 +1,33 @@
+-- Make 4 char indent
+vim.opt.expandtab = true
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.g.mapleader = " "
+vim.g.maplocalleader = "\\"
 
+-- Initiate lazy.nvim
+dofile(vim.fn.stdpath('config') .. '/bootstrap.lua')
+
+-- Setup lazy.nvim
+require("lazy").setup({
+    spec = { -- Plugins
+        {
+            "nyoom-engineering/oxocarbon.nvim",
+            lazy = false,
+            priority = 1000,
+            config = function()
+                vim.opt.background = "dark" -- set this to dark or light
+                vim.cmd("colorscheme oxocarbon")
+            end
+        },
+    },
+
+    install = { -- Additional settings
+        colorscheme = { "oxocarbon" }
+    },
+
+    checker = { -- Automatic updates
+        enabled = true
+    },
+})
